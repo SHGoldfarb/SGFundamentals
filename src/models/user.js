3c1d7ee8c920438fbd8ab5652d77245e589f5b4a
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-09 01:14:10 -0300

    create user model

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+module.exports = function defineuser(sequelize, DataTypes) {
+  const user = sequelize.define('user', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+  user.associate = function associate(models) {
+    // associations can be defined here
+  };
+  return user;
+};
