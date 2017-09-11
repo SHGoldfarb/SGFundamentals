212c5659c35c3e03ca358284efcab3be21dcb6e9
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-11 09:29:12 -0300

    comment users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,14 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+
+// GET /users
 router.get('users', '/', async (ctx) => {
   const users = await ctx.orm.user.findAll();
   await ctx.render('users/index', { users });
 });
 
+// GET /users/new
 router.get('usersNew', '/new', async (ctx) => {
   const user = ctx.orm.user.build();
   await ctx.render('users/new', {
@@ -15,11 +18,18 @@ router.get('usersNew', '/new', async (ctx) => {
   });
 });
 
+// POST /users
 router.post('usersCreate', '/', async (ctx) => {
+  // Create a role
   const role = await ctx.orm.role.create({
     tag: 'user',
   });
+
+  // Add roleId to other params
   ctx.request.body.roleId = role.id;
+
+  // Create user
+  // TODO: catch error
   const user = await ctx.orm.user.create(ctx.request.body);
   ctx.redirect(ctx.router.url('users'));
 });
