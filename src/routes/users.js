408fa47ca6b9e4d716dca1350a679ff7f98685f9
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-10 23:54:56 -0300

    add create user

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,4 +7,21 @@ router.get('users', '/', async (ctx) => {
   await ctx.render('users/index', { users });
 });
 
+router.get('usersNew', '/new', async (ctx) => {
+  const user = ctx.orm.user.build();
+  await ctx.render('users/new', {
+    user,
+    createUserPath: ctx.router.url('usersCreate'),
+  });
+});
+
+router.post('usersCreate', '/', async (ctx) => {
+  const role = await ctx.orm.role.create({
+    tag: 'user',
+  });
+  ctx.request.body.roleId = role.id;
+  const user = await ctx.orm.user.create(ctx.request.body);
+  ctx.redirect(ctx.router.url('users'));
+});
+
 module.exports = router;
