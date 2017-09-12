55a1168ef374a153f61d6d6456cb5eec0ac3912e
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-12 18:12:56 -0300

    fix heroku database try 5

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const config = {
   default: {
     // username: process.env.DB_USERNAME,
     // password: process.env.DB_PASSWORD,
-    // dialect: process.env.DB_DIALECT || 'postgres',
+    dialect: process.env.DB_DIALECT || 'postgres',
     // database: process.env.DB_NAME,
     // host: process.env.DB_HOST || '127.0.0.1',
   },
