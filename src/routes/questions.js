15a879924cb68765134ba36c638133d5db84dda7
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-12 20:48:01 -0300

    finish crud questions

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -22,41 +22,41 @@ router.get('questionsNew', '/new', async (ctx) => {
   });
 });
 
-router.post('excercisesCreate', '/', async (ctx) => {
-  const excercise = await ctx.orm.excercise.create(ctx.request.body);
-  ctx.redirect(ctx.router.url('excercise', { id: excercise.id }));
+router.post('questionsCreate', '/', async (ctx) => {
+  const question = await ctx.orm.question.create(ctx.request.body);
+  ctx.redirect(ctx.router.url('question', { id: question.id }));
 });
 
-router.get('excercise', '/:id', async (ctx) => {
-  const excercise = await ctx.orm.excercise.findById(ctx.params.id);
-  await ctx.render('excercises/show', {
-    excercise,
-    editExcercisePath: ctx.router.url('excercisesEdit', { id: ctx.params.id }),
-    deleteExcercisePath: ctx.router.url('excercisesDelete', { id: ctx.params.id }),
-    backToList: ctx.router.url('excercises'),
+router.get('question', '/:id', async (ctx) => {
+  const question = await ctx.orm.question.findById(ctx.params.id);
+  await ctx.render('questions/show', {
+    question,
+    editQuestionPath: ctx.router.url('questionsEdit', { id: ctx.params.id }),
+    deleteQuestionPath: ctx.router.url('questionsDelete', { id: ctx.params.id }),
+    backToList: ctx.router.url('questions'),
   });
 });
 
-router.get('excercisesEdit', '/:id/edit', async (ctx) => {
-  const excercise = await ctx.orm.excercise.findById(ctx.params.id);
-  await ctx.render('excercises/edit', {
-    excercise,
-    updateExcercisePath: ctx.router.url('excercisesUpdate', { id: ctx.params.id }),
-    deleteExcercisePath: ctx.router.url('excercisesDelete', { id: ctx.params.id }),
-    backToList: ctx.router.url('excercises'),
+router.get('questionsEdit', '/:id/edit', async (ctx) => {
+  const question = await ctx.orm.question.findById(ctx.params.id);
+  await ctx.render('questions/edit', {
+    question,
+    updateQuestionPath: ctx.router.url('questionsUpdate', { id: ctx.params.id }),
+    deleteQuestionPath: ctx.router.url('questionsDelete', { id: ctx.params.id }),
+    backToList: ctx.router.url('questions'),
   });
 });
 
-router.patch('excercisesUpdate', '/:id', async (ctx) => {
-  const excercise = await ctx.orm.excercise.findById(ctx.params.id);
-  await excercise.update(ctx.request.body);
-  ctx.redirect(ctx.router.url('excercise', { id: ctx.params.id }));
+router.patch('questionsUpdate', '/:id', async (ctx) => {
+  const question = await ctx.orm.question.findById(ctx.params.id);
+  await question.update(ctx.request.body);
+  ctx.redirect(ctx.router.url('question', { id: ctx.params.id }));
 });
 
-router.delete('excercisesDelete', '/:id', async (ctx) => {
-  const excercise = await ctx.orm.excercise.findById(ctx.params.id);
-  await excercise.destroy();
-  ctx.redirect(ctx.router.url('excercises'));
+router.delete('questionsDelete', '/:id', async (ctx) => {
+  const question = await ctx.orm.question.findById(ctx.params.id);
+  await question.destroy();
+  ctx.redirect(ctx.router.url('questions'));
 });
 
 module.exports = router;
