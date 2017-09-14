80ceb2ffbdc463a574501c96d87baa51bb4d7978
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-13 23:03:20 -0300

    explain type

--- src/models/comment.js
+++ src/models/comment.js
@@ -2,6 +2,9 @@ module.exports = function definecomment(sequelize, DataTypes) {
   const comment = sequelize.define('comment', {
     content: DataTypes.TEXT,
     parentId: DataTypes.INTEGER,
+    // type: 'answer' si parentId es un ejercicio,
+    //       'reply' si parentId es una respuesta,
+    //       'comment' si parntId es un comentario
     type: DataTypes.STRING,
     userId: DataTypes.INTEGER,
   });
