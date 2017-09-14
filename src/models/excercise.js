332e9646e7e11144778d45f8edd17e5747fd8e57
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-09-14 11:57:05 -0300

    eslint excercise model

--- src/models/excercise.js
+++ src/models/excercise.js
@@ -1,15 +1,13 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var excercise = sequelize.define('excercise', {
+
+module.exports = function defineexcercise(sequelize, DataTypes) {
+  const excercise = sequelize.define('excercise', {
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
-    guide_id:DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+    guide_id: DataTypes.INTEGER,
   });
+  excercise.associate = function associate(models) {
+    // associations can be defined here
+  };
   return excercise;
 };
