b302ce5f654b92f729f8de859fcb4d2fe73261e1
Gabriel Ulloa <gsulloa@uc.cl>
2017-10-03 20:44:31 -0300

    Fixing seed file

--- src/seeds/20170915211136-users.js
+++ src/seeds/20170915211136-users.js
@@ -2,24 +2,19 @@ const faker = require('faker');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const rolesId = (await queryInterface.select(null, 'roles', { attributes: ['id'] }))
-      .map(role => role.id);
-
-    const usersBulkInsertPromises = rolesId.map((role) => {
-      const quantity = role === 1 ? 1 : faker.random.number({ min: 1, max: 15 });
-      const data = [];
-      for (let i = 0; i < quantity; i += 1) {
-        data.push({
-          username: faker.internet.userName(),
-          password: faker.internet.password(),
-          email: faker.internet.email(),
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        });
-      }
-      return queryInterface.bulkInsert('users', data);
-    });
-    return Promise.all(usersBulkInsertPromises);
+    queryInterface.bulkInsert('users', [{
+      email: 'admin@admin',
+      username: 'admin',
+      password: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+    queryInterface.bulkInsert('userRole', [{
+      userId: 1,
+      roleId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
   },
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('users', null, {}),
