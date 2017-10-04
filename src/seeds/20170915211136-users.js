569009224bc56306721eace0ff3aa29c5f636cd5
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-10-04 20:42:24 -0300

    more users

--- src/seeds/20170915211136-users.js
+++ src/seeds/20170915211136-users.js
@@ -2,7 +2,9 @@ const faker = require('faker');
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('users', [{
+    const quantity = 9;
+    const data = [];
+    data.push({
       email: 'admin@admin',
       username: 'admin',
       password: '$2a$13$fDSlLxQUc.7lof75citqbO.9x48dtnXWJvhkXWCCJ81j2nANV5Brq',
@@ -10,15 +12,19 @@ module.exports = {
       actived: true,
       createdAt: new Date(),
       updatedAt: new Date(),
-    }, {
-      email: 'user@user',
-      username: 'user',
-      password: '$2a$13$Gt1AxSg3kTE2Um4TgEGexOaLgiFR4MJ3vLU6UQnxlDXvFUm0KlI5m',
-      activeToken: null,
-      actived: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }]);
+    });
+    for (let i = 0; i < quantity; i += 1) {
+      data.push({
+        email: `user${i + 1}@users`,
+        username: `user${i + 1}`,
+        password: '$2a$13$Gt1AxSg3kTE2Um4TgEGexOaLgiFR4MJ3vLU6UQnxlDXvFUm0KlI5m',
+        activeToken: null,
+        actived: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    return queryInterface.bulkInsert('users', data);
   },
 
   down(queryInterface, Sequelize) {
--- src/seeds/20170915211137-userRoles.js
+++ src/seeds/20170915211137-userRoles.js
@@ -2,22 +2,24 @@ const faker = require('faker');
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('userroles', [{
+    const quantity = 10;
+    const data = [];
+    data.push({
       userId: 1,
       roleId: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
-    }, {
-      userId: 1,
-      roleId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      userId: 2,
-      roleId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }]);
+    });
+    for (let i = 0; i < quantity; i += 1) {
+      data.push({
+        userId: i + 1,
+        roleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+    return queryInterface.bulkInsert('userroles', data);
   },
 
   down(queryInterface, Sequelize) {
