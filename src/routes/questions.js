fe97453ce32d7e8ecd5498b436928c7b853f48ff
Samuel Goldfarb <shgoldfarb@uc.cl>
2017-10-04 23:14:11 -0300

    better question nav

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -56,6 +56,7 @@ router.get('question', '/:id', async (ctx) => {
     createTagPath: ctx.router.url('tagsCreate'),
     buildTagDeletePath: id => ctx.router.url('tagsDelete', { id }),
     buildCommentDeletePath: id => ctx.router.url('commentsDelete', { id }),
+    buildCommentPath: id => ctx.router.url('comment', { id }),
   });
 });
 
