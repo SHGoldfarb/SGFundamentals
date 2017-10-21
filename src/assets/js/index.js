840c4fdeb3a5fe2ae2694e7c0c223b74cda9bb42
Gabriel Ulloa <gsulloa@uc.cl>
2017-10-21 13:54:18 -0300

    Eliminando mensaje al cerrar

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,7 @@ function closeError() {
   const error = document.getElementById('error_message');
   const body = document.getElementsByTagName('BODY')[0];
   error.classList.add('hidden');
+  setTimeout(() => body.removeChild(error), 700);
 }
 
 if (document.getElementById('close')) {
