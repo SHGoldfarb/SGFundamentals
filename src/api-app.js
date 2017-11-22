d823b626bb6a71177e092bd6d5df13ec9ab7ccb9
Gabriel Ulloa <gsulloa@uc.cl>
2017-11-22 00:07:58 -0300

    CORS config

--- src/api-app.js
+++ src/api-app.js
@@ -1,10 +1,11 @@
 const Koa = require('koa');
+const cors = require('koa-cors');
 const routes = require('./api-routes.js');
 const jsonApiSerializer = require('jsonapi-serializer');
 
 // App constructor
 const app = new Koa();
-
+app.use(cors());
 app.use((ctx, next) => {
   ctx.jsonSerializer = function jsonSerializer(type, options) {
     return new jsonApiSerializer.Serializer(type, options);
